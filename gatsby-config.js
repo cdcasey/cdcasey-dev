c8adb63d682dfbfaf52b46ac9b9c4cfc87a7106e
Chris Casey <cdcasey@gmail.com>
2020-07-22 22:15:58 -0500

    Adding TypeScript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,25 @@
 const siteMetadata = {
-  title: 'How I made this site and other thoughts.',
-  description:
-    'I write down helpful tips so that I remember them. You benefit. Win-win.',
-  author: '@cdcasey, Chris Casey',
+  title: `How I made this site and other thoughts.`,
+  description: `I write down helpful tips so that I remember them. You benefit. Win-win.`,
+  author: `@cdcasey, Chris Casey`,
 };
 
 const plugins = [
   {
-    resolve: 'gatsby-plugin-mdx',
+    resolve: `gatsby-plugin-mdx`,
     options: {
-      extensions: ['.mdx', '.md'],
+      extensions: [`.mdx`, `.md`],
     },
   },
   {
-    resolve: 'gatsby-source-filesystem',
+    resolve: `gatsby-source-filesystem`,
     options: {
       path: `${__dirname}/content`,
-      name: 'content',
+      name: `content`,
     },
   },
-  'gatsby-plugin-emotion',
+  `gatsby-plugin-emotion`,
+  `gatsby-plugin-typescript`,
 ];
 
 module.exports = {
