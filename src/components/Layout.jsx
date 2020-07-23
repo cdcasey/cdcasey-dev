671c6de6c25c7d807585b376ab712612132f82d9
Chris Casey <cdcasey@gmail.com>
2020-07-22 22:20:17 -0500

    Prettifying

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import styled from '@emotion/styled'
+import React from 'react';
+import styled from '@emotion/styled';
 
-import { useSiteMetadata } from '../hooks/useSiteMetadata'
-import { Header } from './Header'
+import { useSiteMetadata } from '../hooks/useSiteMetadata';
+import { Header } from './Header';
 
 const AppStyles = styled.main`
-    width: 800px;
-    margin: 0 auto;
-`
+  width: 800px;
+  margin: 0 auto;
+`;
 
 export function Layout({ children }) {
-    const { title, description } = useSiteMetadata()
+  const { title, description } = useSiteMetadata();
 
-    return (
-        <AppStyles>
-            <Header siteTitle={title} siteDescription={description} />
-            {children}
-        </AppStyles>
-    )
+  return (
+    <AppStyles>
+      <Header siteTitle={title} siteDescription={description} />
+      {children}
+    </AppStyles>
+  );
 }
--- src/hooks/useSiteMetadata.js
+++ src/hooks/useSiteMetadata.js
@@ -12,7 +12,7 @@ export function useSiteMetadata() {
           }
         }
       }
-    `
+    `,
   );
   return site.siteMetadata;
 }
