ee78c7496386017781f1af797b71a6dab90f3a28
Chris Casey <cdcasey@gmail.com>
2020-07-31 22:01:23 -0500

    Copy snippets to clipboard

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -3,7 +3,10 @@ import Highlight, { defaultProps } from 'prism-react-renderer';
 import styled from '@emotion/styled';
 import theme from 'prism-react-renderer/themes/nightOwl';
 
+import { copyToClipboard } from '../utils/copy-to-clipboard';
+
 export const Pre = styled.pre`
+  position: relative;
   text-align: left;
   margin: 1em auto;
   padding: 0.5em;
@@ -16,6 +19,19 @@ export const Pre = styled.pre`
   }
 `;
 
+export const CopyCode = styled.button`
+  position: absolute;
+  right: 0.25rem;
+  border: 0;
+  border-radius: 3px;
+  margin: 0.25rem;
+  opacity: 0.3;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
 export const LineNo = styled.span`
   display: inline-block;
   width: 2em;
@@ -24,10 +40,15 @@ export const LineNo = styled.span`
 `;
 
 export const Code = ({ codeString, language }) => {
+  const handleClick = () => {
+    copyToClipboard(codeString);
+  };
+
   return (
     <Highlight {...defaultProps} code={codeString} language={language} theme={theme}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <Pre className={className} style={style}>
+          <CopyCode onClick={handleClick}>Copy</CopyCode>
           {tokens.map((line, i) => (
             <div key={line} {...getLineProps({ line, key: i })}>
               <LineNo>{i + 1}</LineNo>
