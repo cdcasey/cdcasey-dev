4def5bc4c53507b972a145d4b595f137a22f3a45
Chris Casey <cdcasey@gmail.com>
2020-08-02 22:19:19 -0500

    Some updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  // parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
     // 'airbnb',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
@@ -8,7 +12,7 @@ module.exports = {
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
