e6ead061d6744bb6ecd0089d4fb4a74eb402dcfe
Chris Casey <cdcasey@users.noreply.github.com>
2024-06-28 21:42:39 -0500

    some editor setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://cdcasey.dev',
 	integrations: [mdx(), sitemap()],
 });
